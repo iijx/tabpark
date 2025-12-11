eb19cd241e9eeef3602b572bf36988ef09d4cc6e
lihuan <iijx@qq.com>
2025-12-11 13:56:19 +0800

    add affine

--- src/modules/affine/index.vue
+++ src/modules/affine/index.vue
@@ -1,6 +1,12 @@
 <template>
   <main class="w-full h-full relative">
-    <iframe src="https://affine.tabpark.cn/app" frameborder="0" width="100%" height="100%"></iframe>
+    <iframe
+      src="https://affine.tabpark.cn"
+      frameborder="0" 
+      width="100%" 
+      height="100%" 
+      allow="autoplay; fullscreen; geolocation; microphone; camera; *"
+    />
   </main>
 </template>
 
--- src/modules/mdcard/index.vue
+++ src/modules/mdcard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <main class="w-full h-full relative">
-    <iframe src="https://opennotas.tabpark.cn/app" frameborder="0" width="100%" height="100%"></iframe>
+    <iframe 
+      src="https://opennotas.tabpark.cn/app" 
+      frameborder="0" 
+      width="100%" 
+      height="100%" 
+      allow="autoplay; fullscreen; geolocation; microphone; camera; *"
+    />
   </main>
 </template>
 
